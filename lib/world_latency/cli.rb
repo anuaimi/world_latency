623ad62ea64ded4465ea108d290a027bf51816da
Athir Nuaimi <athir@nuaimi.com>
2014-09-14 11:14:04 -0400

    interim work on list class

--- lib/world_latency/cli.rb
+++ lib/world_latency/cli.rb
@@ -1,12 +1,46 @@
 require 'optparse'
 
 module WorldLatency
-	class CLI
-		def initialize
-		end
+  class CLI
+    def initialize
 
-		def execute
-		end
-		
-	end
+      # process command line options
+      options = { source: 'localhost', database: nil}
+      parser = optparse = OptionParser.new do |opts|
+        opts.on( '-s', '--source NAME', "Name of source of pings") do |name|
+          options[:source] = name
+        end
+        opts.on( '-d', '--database FILENAME', "File with country database") do |filename|
+          options[:database] = filename
+        end
+      end
+      parser.parse!
+
+      # create global pinger object & start the ping test
+      pinger = GlobalPinger.new( options[:source], options[:database])
+      result, ping_times = pinger.ping
+
+      # create JSON structure
+      results = {}
+      results[:source] = options[:source]
+      results[:times] = ping_times
+
+      # save results
+      filename = './output/' + options[:source] + '.json'
+      File.open( filename, 'w') do |f|
+        f.write results.to_json
+      end
+      #puts results.to_json
+
+      # done
+
+
+
+    end
+
+    def execute
+    end
+
+  end
 end
+
